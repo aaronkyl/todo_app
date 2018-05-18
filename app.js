e1228d60445205705f255813ad1f7bf40702cd95
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-18 02:12:12 +0000

    finishing implementation

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var nunjucks = require('nunjucks');
-var pgp = require('pg-promise')({});
-var db = pgp({database: 'todo', user: 'postgres'});
+const express = require('express');
+const nunjucks = require('nunjucks');
+const body_parser = require('body-parser');
+const pgp = require('pg-promise')({});
+const db = pgp({database: 'todo', user: 'postgres'});
 
-var app = express();
+const app = express();
+
+app.use(body_parser.urlencoded({extended: false}));
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -12,12 +15,41 @@ nunjucks.configure('views', {
 });
 
 app.get('/todos', function(request, response) {
-    db.any("SELECT * FROM task")
+    db.any('SELECT * FROM task')
     .then(function(results) {
-        response.render('base.html', {tasks: results});
+        response.render('index.html', {tasks: results});
     })
     .catch(function(error) {
         console.log(error);
+    });
+});
+
+app.get('/todos/add', function(request, response) {
+    response.render('add.html', {});
+});
+
+app.get('/todos/:id', function(request, response) {
+    let id = request.params.id;
+    db.none('UPDATE task SET done = True WHERE id = $1', [id])
+    .then(function() {
+        response.redirect('/todos');
+    })
+    .catch(function(error) {
+        console.log("**ERROR** ", error);
+        response.redirect('/todos');
+    });
+});
+
+app.post('/add', function(request, response) {
+    console.log(request.body);
+    let desc = request.body.description;
+    db.none('INSERT INTO task VALUES (default, $1, False)', [desc])
+    .then(function() {
+        response.redirect('/todos');
+    })
+    .catch(function(error) {
+        console.log("**ERROR** ", error);
+        response.redirect('/todos');
     });
 });
 
